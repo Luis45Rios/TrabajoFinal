6e83adbe93fa871c4a76755f81b5236f1085fe89
dome-rojas <domenica.rojas@unl.edu.ec>
2024-12-06 11:44:16 -0500

    Modificación del archivo App

--- src/views/App.java
+++ src/views/App.java
@@ -1,6 +1,26 @@
 package views;
+
+import java.util.Scanner;
+
+import util.Pomodoro;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        @SuppressWarnings("resource") // Suprime las advertencias
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("BIENVENIDO AL POMODORO");
+
+        System.out.println("Elige el nombre para tu tarea:");
+        String nombreTarea = scanner.nextLine();
+
+        System.out.println("¿Qué nivel te vas a dedicar?");
+        System.out.println("(1) Enfocado = 25 min");
+        System.out.println("(2) Muy Enfocado = 50 min");
+        int enfoque = scanner.nextInt();
+
+        Pomodoro pomodoro = new Pomodoro(nombreTarea, enfoque);
+        pomodoro.comenzarPomodoro(scanner);
     }
 }
