5c51a1b1efd0be2a6c6eff7196bfae1b18d9f3ef
Luis <luis.h.rios@unl.edu.ec>
2024-12-06 10:47:51 -0500

    Iniciar el Pomodotro

--- src/util/Pomodoro.java
+++ src/util/Pomodoro.java
@@ -6,6 +6,22 @@ public class Pomodoro {
     private int TIEMPO_DESCANSO_CORTO = 0;
     private int TIEMPO_DESCANSO_LARGO = 0;
     private int CICLOS_COMPLETOS = 0;
-                
-    
+
+    public Pomodoro(String tarea, int enfoque) {
+        // INICIALIZADOR DE LA CLASE
+        this.TAREA = tarea;
+        switch (enfoque) {
+            case 1:
+                this.TIEMPO_POMODORO = 25;
+                this.TIEMPO_DESCANSO_CORTO = 5;
+                this.TIEMPO_DESCANSO_LARGO = 15;
+                break;
+            default:
+                System.out.println("Opción no válida. Se le asignará un enfoque por defecto");
+                this.TIEMPO_POMODORO = 25;
+                this.TIEMPO_DESCANSO_CORTO = 5;
+                this.TIEMPO_POMODORO = 15;
+                break;
+        }
+    }
 }
