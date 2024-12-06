190f43f0635d8273711d69182a696c842987003e
Steven Jumbo <stevenalexanderjumbojaramillo@gmail.com>
2024-12-06 11:22:05 -0500

    Inicialización del constructor Pomodoro

--- src/util/Pomodoro.java
+++ src/util/Pomodoro.java
@@ -1,26 +1,31 @@
 package util;
 
 public class Pomodoro {
-    private String TAREA = "";
-    private int TIEMPO_POMODORO = 0;
-    private int TIEMPO_DESCANSO_CORTO = 0;
-    private int TIEMPO_DESCANSO_LARGO = 0;
-    private int CICLOS_COMPLETOS = 0;
+    private String tarea;
+    private final int TIEMPO_POMODORO;
+    private final int TIEMPO_DESCANSO_CORTO;
+    private final int TIEMPO_DESCANSO_LARGO;
+    private int CICLOS_COMPLETOS;
 
     public Pomodoro(String tarea, int enfoque) {
         // INICIALIZADOR DE LA CLASE
-        this.TAREA = tarea;
+        this.tarea = tarea;
         switch (enfoque) {
             case 1:
                 this.TIEMPO_POMODORO = 25;
                 this.TIEMPO_DESCANSO_CORTO = 5;
                 this.TIEMPO_DESCANSO_LARGO = 15;
                 break;
+            case 2:
+                this.TIEMPO_POMODORO = 50;
+                this.TIEMPO_DESCANSO_CORTO = 10;
+                this.TIEMPO_DESCANSO_LARGO = 30;
+                break;
             default:
-                System.out.println("Opción no válida. Se le asignará un enfoque por defecto");
+                System.out.println("Sólo opciones 1 y 2. Se le asignará un enfoque por defecto");
                 this.TIEMPO_POMODORO = 25;
                 this.TIEMPO_DESCANSO_CORTO = 5;
-                this.TIEMPO_POMODORO = 15;
+                this.TIEMPO_DESCANSO_LARGO = 15;
                 break;
         }
     }
