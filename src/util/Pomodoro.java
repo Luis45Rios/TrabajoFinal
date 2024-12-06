f221322ed35f974a07708aa5317b6fa66e7b69cf
Steven Jumbo <stevenalexanderjumbojaramillo@gmail.com>
2024-12-06 09:16:35 -0500

    Inicializacion de costantes para Pomodoro

--- src/util/Pomodoro.java
+++ src/util/Pomodoro.java
@@ -1,5 +1,11 @@
 package util;
 
+import java.util.PrimitiveIterator;
+
 public class Pomodoro {
-    
+    private final String TAREA = "";
+    private final int TIEMPO_POMODORO = 0;
+    private final int TIEMPO_DESCANSO_CORTO = 0;
+    private final int TIEMPO_DESCANSO_LARGO = 0;
+    private int CICLOS_COMPLETOS = 0;
 }
